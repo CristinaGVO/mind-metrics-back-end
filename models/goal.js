118cd4f789fd3737cb29989306b7dfb053e99d70
Thais Palma de Brito <britop.tech@gmail.com>
2026-01-23 09:46:14 -0800

    add required field for userId in schemas

--- models/goal.js
+++ models/goal.js
@@ -5,6 +5,7 @@ const goalSchema = new mongoose.Schema(
         userId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
+            required: true,
         },
         title: {
             type: String,
